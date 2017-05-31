fd8ab92a7c86f6de4bc3205f5de77cb82c12e5b4
Robert Weiss <weiszr@vt.edu>
2017-05-31 15:52:11 -0400

    found antidunes window

--- complex.py
+++ complex.py
@@ -90,11 +90,13 @@ def message():
 #F=linspace(0.5,2.0,100)
 
 #F=0.8
-mn_n = 100
-Froude = random_floats(0.5,1.5,mn_n)
-kd = random_floats(1.0,2.0,mn_n)
-j = random_floats(0.5,4.0,mn_n)
-dd = random_floats(5.0,12.0,mn_n)
+mn_n = 2000
+Froude = random_floats(0.5,2.0,mn_n)
+kd = random_floats(4.0,4.0,mn_n)
+j = random_floats(0.8,0.8,mn_n)
+dd = random_floats(2.0,2.0,mn_n)
+Froude = linspace(0.5,2.5,mn_n)
+kd = linspace(0.5,2.5,mn_n)
 nt=100
 nx=500
 A_o=0.6
@@ -102,7 +104,7 @@ A_o=0.6
 #vel = 0.5
 dg = 1.0 * 1.0e-3
 w_s = ferg(dg)
-vel_c = usstarcr(dg)*1.0
+vel_c = usstarcr(dg)*10.0
 print "vel", vel_c
 #j = 2.0
 depth = 1.0
@@ -118,62 +120,127 @@ depth = 1.0
 
 m = 0.5
 n = 2.64
-ad_x = []
-ad_y = []
-nd_x = []
-nd_y = []
+sad_x = []
+sad_y = []
+sad_z = []
+sad_v = []
+snd_x = []
+snd_y = []
+snd_z = []
+snd_v = []
 
-for kk in range(mn_n):
-    F=Froude[kk]
-    depth=dd[kk]
-    vel=F*sqrt(9.81*depth)
-    length = 2.0*pi*depth/kd[kk]
-    k =  2. * pi / length
-    delta = j[kk]* depth * 2.* pi / k
-    u_b = calc_u_b(vel,vel_c,F,k,depth,delta,m,n)
-    print u_b,kd[kk],j[kk],Froude[kk]
+uad_x = []
+uad_y = []
+uad_z = []
+uad_v = []
+und_x = []
+und_y = []
+und_z = []
+und_v = []
 
-    t = linspace(0,30.0,nt)
-    dt = t[1]-t[0]
-    a= zeros(nt)
-    a[0] = A_o/1.
-    for i in range(1,nt):
-        term1 = (tanh(k * depth) - (F**2. * k * depth)**(-1.)) / (1. - tanh(k *depth)/(F**2.0 * k * depth))
-        a[i]=a[i-1]- dt*(a[i-1] * calc_t_bar(vel,vel_c,m,n) * k**2.0 * vel / (vel - vel_c) *term1 * sin(k * delta))
+for kk1 in range(mn_n):
+    for kk2 in range(mn_n):
+        F=Froude[kk1]
+        depth=dd[kk1]
+        vel=F*sqrt(9.81*depth)
+        length = 2.0*pi*depth/kd[kk2]
+        k =  2. * pi / length
+        delta = j[kk1]* length
+        u_b = calc_u_b(vel,vel_c,F,k,depth,delta,m,n)
+        print u_b,kd[kk1],j[kk1],Froude[kk1]
 
-# a =  calc_a(t,vel,vel_c,F,k,depth,delta,m,n,A_o)
-    x = linspace(0,4.0*length,nx)
-    eta = zeros([nt,nx])
-    xi = zeros([nt,nx])
-    maxeta = []
-    maxxi = []
-    maxdistance_eta = []
-    maxdistance_xi = []
-    for tt in range(nt):
-        #print a[tt],u_b,F,k*delta,delta/depth,F**2.0*k*depth*tanh(k*depth)
-        for ii in range(nx):
-            eta[tt,ii] = -depth + a[tt] * sin(k*(x[ii]-u_b*t[tt]))
-            xi[tt,ii] = sin(k*(x[ii]-u_b*t[tt]))*a[tt] / ((1.0 - tanh(k * depth)/(F**2.0*k*depth))*cosh(k *depth))
-    if a[10] == a[11]:# and a[10]/a[11]<1.05:
-        if u_b < 0.0:
-            ad_x.append(dd[kk])
-            ad_y.append(Froude[kk])
-        if u_b > 0.0:
-            nd_x.append(dd[kk])
-            nd_y.append(Froude[kk])
-nd_x = array(nd_x)
-nd_y = array(nd_y)
-ad_x = array(ad_x)
-ad_y = array(ad_y)
-ar3=column_stack((nd_x,nd_y))
-ar4=column_stack((ad_x,ad_y))
+        t = linspace(0,30.0,nt)
+        dt = t[1]-t[0]
+        a= zeros(nt)
+        a[0] = A_o/1.
+        for i in range(1,nt):
+            term1 = (tanh(k * depth) - (F**2. * k * depth)**(-1.)) / (1. - tanh(k *depth)/(F**2.0 * k * depth))
+            a[i]=a[i-1]- dt*(a[i-1] * calc_t_bar(vel,vel_c,m,n) * k**2.0 * vel / (vel - vel_c) *term1 * sin(k * delta))
 
-outfile1=open('norm_dunes.dat','w')
-outfile2=open('anti_dunes.dat','w')
+    # a =  calc_a(t,vel,vel_c,F,k,depth,delta,m,n,A_o)
+        # x = linspace(0,4.0*length,nx)
+        # eta = zeros([nt,nx])
+        # xi = zeros([nt,nx])
+        # maxeta = []
+        # maxxi = []
+        # maxdistance_eta = []
+        # maxdistance_xi = []
+        # for tt in range(nt):
+        # #print a[tt],u_b,F,k*delta,delta/depth,F**2.0*k*depth*tanh(k*depth)
+        #     for ii in range(nx):
+        #         eta[tt,ii] = -depth + a[tt] * sin(k*(x[ii]-u_b*t[tt]))
+        #         xi[tt,ii] = sin(k*(x[ii]-u_b*t[tt]))*a[tt] / ((1.0 - tanh(k * depth)/(F**2.0*k*depth))*cosh(k *depth))
+        # noinspection PyTypeChecker
+        if 0.90 < a[10]/a[70] < 1.1:
+            if u_b < 0.0:
+                sad_x.append(dd[kk1])
+                sad_y.append(Froude[kk1])
+                sad_z.append(j[kk1])
+                sad_v.append(kd[kk2])
+            if u_b >= 0.0:
+                snd_x.append(dd[kk1])
+                snd_y.append(Froude[kk1])
+                snd_z.append(j[kk1])
+                snd_v.append(kd[kk2])
+        # noinspection PyTypeChecker
+        else:
+            if u_b < 0.0:
+                uad_x.append(dd[kk1])
+                uad_y.append(Froude[kk1])
+                uad_z.append(j[kk1])
+                uad_v.append(kd[kk2])
+            if u_b >= 0.0:
+                und_x.append(dd[kk1])
+                und_y.append(Froude[kk1])
+                und_z.append(j[kk1])
+                und_v.append(kd[kk2])
+
+und_x = array(und_x)
+und_y = array(und_y)
+und_z = array(und_z)
+und_v = array(und_v)
+
+uad_x = array(uad_x)
+uad_y = array(uad_y)
+uad_z = array(uad_z)
+uad_v = array(uad_v)
+
+snd_x = array(snd_x)
+snd_y = array(snd_y)
+snd_z = array(snd_z)
+snd_v = array(snd_v)
+
+sad_x = array(sad_x)
+sad_y = array(sad_y)
+sad_z = array(sad_z)
+sad_v = array(sad_v)
+
+#
+
+ar3=column_stack((snd_x,snd_y,snd_z,snd_v))
+ar4=column_stack((sad_x,sad_y,sad_z,sad_v))
+
+ar5=column_stack((und_x,und_y,und_z,und_v))
+ar6=column_stack((uad_x,uad_y,uad_z,uad_v))
+
+outfile1=open('snorm_dunes.dat','w')
+outfile2=open('santi_dunes.dat','w')
+outfile3=open('unorm_dunes.dat','w')
+outfile4=open('uanti_dunes.dat','w')
 savetxt(outfile1, ar3,fmt='%6.3e')
 savetxt(outfile2, ar4,fmt='%6.3e')
+savetxt(outfile3, ar5,fmt='%6.3e')
+savetxt(outfile4, ar6,fmt='%6.3e')
 outfile1.close()
 outfile2.close()
+outfile3.close()
+outfile4.close()
+
+print 'Length Stable Antidune', len(sad_v)
+print "Length Unstable Antidudes",len(uad_v)
+
+print 'Length Stable Dunes', len(snd_v)
+print 'Length Unstable Dunes',len(und_v)
 # for tt in range(nt):
 #     for ii in range(0,nx-2):
 #         if x[ii]>0.25*length and eta[tt,ii+1]< eta[tt,ii]:
@@ -217,4 +284,4 @@ outfile2.close()
 # plot(ad_x,ad_y,'bo')
 # plot(nd_x,nd_y,'ro')
 # show()
-print len(ad_x)
+# print len(ad_x)
